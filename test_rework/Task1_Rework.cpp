0b2014ef1ccab23684f5b8790426049bbbb29c02
silvitreatment <1junyawork@gmail.com>
2024-11-18 21:22:51 +0300

    Added implementation of task1 from the test

--- test_rework/Task1_Rework.cpp
+++ test_rework/Task1_Rework.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 
-
 int main() {
     
     const int arr[] = {3, 6, 5, 9, 10};
     const int size = sizeof(arr) / sizeof(arr[0]);
-
- 
     int maxNum = arr[0];
     int minNum = arr[0];
     
@@ -21,13 +18,11 @@ int main() {
 
     double ratio = double(maxNum) /  minNum;
 
-
     std::cout << "Массив: ";
     for (int num : arr) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
-
     std::cout << "Максимальное значение: " << maxNum << std::endl;
     std::cout << "Минимальное значение: " << minNum << std::endl;
     std::cout << "Во сколько раз максимальное больше минимального: " << ratio << std::endl;
